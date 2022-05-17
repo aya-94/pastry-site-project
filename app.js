e7f8f04bb2ea1e14dd287dd338768209d6c02336
hoday <hodayamirr@gmail.com>
2022-05-17 13:41:07 +0300

    commit

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
 
+// active menu
 menu.addEventListener('click', () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
 
+// tilt.js on photos
 VanillaTilt.init(document.querySelectorAll('.js-tilt'), {
     scale: 1.2
 });
